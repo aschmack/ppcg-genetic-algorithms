0e17de91e682433ee38d3d7d512816b8883553e6
Adam <aschmack@gmail.com>
2015-01-20 21:05:13 -0500

    Striped-bits and new LUTs oh my
    Switched to striping the bits to generate the score. Idea is that a
    crossover would equally screw up/help the score for all colors. Changed
    the Sum LUTs so that we weigh the immediate directional over the
    look-ahead.

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,27 +1,4 @@
 #include "./gamelogic.cpp"
-#include <string.h>
-#include <fstream>
-
-#define ID_BITS 4
-#define SCORE_BITS 3
-#define TOTAL_BITS (ID_BITS+SCORE_BITS)
-#define MAX_SCORE ((int)pow(SCORE_BITS,2)-1)
-
-// label that may not be used
-enum direction_lut {
-	UP_RIGHT=0, RIGHT, DOWN_RIGHT
-};
-
-// score index arrays for the 8 movement directions
-static const int sum_lut[3][6] = {
-	{ 2, 3, 4, 8, 9, 14 }, { 8, 9, 13, 14, 18, 19 },
-	{ 14, 18, 19, 22, 23, 24 }
-};
-
-// return values corresponding to the movement directions
-static const coord_t direction_lut[3] = {
-	{ 1, -1 }, { 1, 0 }, { 1, 1 }
-};
 
 //takes (len) bits of DNA as a binary number
 int dnarange(dna_t &d, int start, int len) {
@@ -32,33 +9,48 @@ int dnarange(dna_t &d, int start, int len) {
     return res;
 }
 
-//go through our "knowledge" and make a map from color->score for
-//quick lookup
-void mapColors(dna_t &d, int* color_array=NULL) {
-	for (int bits = 0; bits < DNA_BITS-TOTAL_BITS; bits += TOTAL_BITS) {
-		int color_id = dnarange(d,bits,ID_BITS);
-		int color_knowledge = (int)dnarange(d,bits+ID_BITS,SCORE_BITS);
-		
-		// If it falls on an odd bit then it is negative knowledge
-		if ( (bits+ID_BITS)%2 == 0 )
-			color_array[color_id]+=color_knowledge;
-		else
-			color_array[color_id]-=color_knowledge;
+// get striped color score, 6 bits per color. should be
+// resistant to getting erased by a crossover
+void mapColorsBitwise(dna_t &d, int* color_array) {
+	for (int i=0; i<N_COLORS; i++) {
+		int score = 0;
+		for (int j=0; j<6; j++) {
+			score = (score<<1) | d[ j*N_COLORS + i ];
+		}
+		color_array[i] = score;
 	}
 }
 
+// label for the lookup tables
+enum direction_lut {
+	UP_RIGHT=0, RIGHT, DOWN_RIGHT
+};
+
+// movement coord_t's to correspond to a direction
+static const coord_t direction_lut[3] = {
+	{ 1, -1 }, { 1, 0 }, { 1, 1 }
+};
+
+// indexes into the arrays to denote what should be summed
+// for each direction.
+static const int sum_lut[3][6] = {
+	{ 3, 4, 8, 8, 9, 14 }, { 9, 13, 13, 14, 14, 19 },
+	{ 14, 18, 18, 19, 23, 24 }
+};
+
 coord_t lookAheadPlayer(dna_t d, view_t v) {
 	int scoreArray[25] = { 0 };
-	int seenColors[N_COLORS] = { };
-	mapColors(d, seenColors);
-
+	int colorScores[N_COLORS] = { };
+	
+	// Get color mapping for this iteration
+	mapColorsBitwise(d, colorScores);
 		
 	for (int y=-2; y<=2; y++) {
 		for (int x=0; x<=2; x++) {
 			// Get the scores for our whole field of view
 			color_t color = v(x,y);
 			if (color != OUT_OF_BOUNDS)
-				scoreArray[ (x+2)+((y+2)*5) ] += seenColors[color];
+				scoreArray[ (x+2)+((y+2)*5) ] += colorScores[color];
 		}
 	}
 	
@@ -85,5 +77,5 @@ coord_t lookAheadPlayer(dna_t d, view_t v) {
 }
 
 int main() {
-    slog << "Look ahead score: " << runsimulation(lookAheadPlayer);
+    slog << "LookAheadPlayer score: " << runsimulation(lookAheadPlayer);
 }
